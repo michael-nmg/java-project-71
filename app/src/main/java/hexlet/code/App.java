edb2f39e07928c06c3eaa4921729996a7683b77d
michael-nmg <michael.nmg@gmail.com>
2023-08-28 15:35:49 +0300

    fix App.java: add picocli logic

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,24 @@
 package hexlet.code;
 
-class App {
-    public static void main(String[] args) {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "Generate differences, version 1.0",
+        description = "Compares two configuration files and shows a difference.")
+class App implements Callable<Integer> {
+
+    @Override
+    public Integer call() {
         System.out.println("Hello World!");
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
